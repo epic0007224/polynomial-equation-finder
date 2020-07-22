c254be96e43d68827784f369424b5e61925c0c4a
Oscar Mackenzie <oscarmac384@gmail.com>
2020-07-23 09:07:47 +1000

    Add files via upload

--- find_equation.py
+++ find_equation.py
@@ -1,47 +1,50 @@
 import sympy as sp
 
-def get_matrix_points():
-    matrix_points = []
+class FindPolynomial:
+    def __init__(self, x_values, y_values):
+        self.x_values = x_values
+        self.y_values = y_values
+        
+    def get_matrix_points(self):
+        matrix_points = []
 
-    for i in range(len(x_values)):
-        matrix_points.append([])
-        for j in range(len(x_values)-1, -1, -1):
-            matrix_points[-1].append(x_values[i]**j)
-        matrix_points[-1].append(y_values[i])
+        for i in range(len(self.x_values)):
+            matrix_points.append([])
+            for j in range(len(self.x_values)-1, -1, -1):
+                matrix_points[-1].append(self.x_values[i]**j)
+            matrix_points[-1].append(self.y_values[i])
 
-    return matrix_points
+        return matrix_points
 
-def get_constants(matrix):
-    rref = list(matrix.rref()[0])
-    constants = [rref[i] for i in range(len(x_values), len(rref), len(x_values)+1)]
+    def get_constants(self, matrix):
+        rref = list(matrix.rref()[0])
+        constants = [rref[i] for i in range(len(self.x_values), len(rref), len(self.x_values)+1)]
 
-    return constants
+        return constants
 
-def create_answer(constants):
-    answer = ''
-    for i in range(len(x_values)-1, -1, -1):
-        num = constants[len(x_values)-1-i]
-        if num != 0:
-            if i == 0:
-                answer += f'{constants[-1]}'
-            else:
-                answer += f'({num})x^{i} + '
+    def create_answer(self, constants):
+        answer = ''
+        for i in range(len(self.x_values)-1, -1, -1):
+            num = constants[len(self.x_values)-1-i]
+            if num != 0:
+                if i == 0:
+                    answer += f'{constants[-1]}'
+                else:
+                    answer += f'({num})x^{i} + '
 
-    return answer
+        return answer
 
-def find_polynomial():
-    matrix = sp.Matrix(get_matrix_points())
+    def find(self):
+        matrix = sp.Matrix(self.get_matrix_points())
 
-    constants = get_constants(matrix)
-    answer = create_answer(constants)
+        constants = self.get_constants(matrix)
+        answer = self.create_answer(constants)
 
-    return answer
+        return answer
 
 if __name__ == '__main__':
-    global x_values
-    global y_values
     x_values = list(map(int, input('Enter the x values: ').split(' ')))
     y_values = list(map(int, input('Enter the y values: ').split(' ')))
     
-    answer = find_polynomial()
+    answer = FindPolynomial(x_values, y_values).find()
     print(answer)
